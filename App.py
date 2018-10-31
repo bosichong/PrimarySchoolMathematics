04bfc1daa3df0791789d664c517cf67b630477e3
J_sky <bosichong@qq.com>
2018-10-31 22:08:43 +0800

    修复了一些BUG，修改了一些__is_valid方法里判断的逻辑，GUI还在布局中。

--- App.py
+++ App.py
@@ -8,13 +8,98 @@
 
 '''
 import random
+import tkinter as tk
+from tkinter import ttk
 
 from Psmrcddup import Generator
 from PrintPreview import PrintPreview
 
 
+
+
+
+
+
+def cdef():
+    if var.get():
+        strvar.set("看，我改变了！")
+    else:
+        strvar.set("有种你点我试试")
+
+
+def rdoprt():
+    print('...')
+
+
+
+
+
+def go(*args):   #处理事件，*args表示可变参数
+        print(comboxlist.get()) #打印选中的值
+
+
+
+root = tk.Tk()#创建一个root窗口
+root.title('基于Python开发的小学生口算题生成器')#设置窗口标题
+top_frame = tk.Frame(root)
+top_frame.pack(fill=tk.X,)
+
+
+t_frame = tk.LabelFrame(top_frame, text="口算题类型选择", padx=5, pady=5)
+t_frame.pack(fill=tk.X, side=tk.TOP)
+
+
+c_frame = tk.LabelFrame(top_frame, text="口算题详细设置", padx=5, pady=5)
+c_frame.pack(fill=tk.X, side=tk.TOP)
+
+l_frame = tk.LabelFrame(c_frame, text="进退位设置", padx=5, pady=5)
+l_frame.pack(fill=tk.X, side=tk.TOP)
+
+
+
+
+###########口算题类型选择############
+vara = tk.StringVar()
+vara.set("L")  # initialize
+ra1=tk.Radiobutton(t_frame,text='加法',variable=vara,value='1',command=rdoprt)
+ra1.pack(anchor=tk.W,side=tk.LEFT)
+ra2=tk.Radiobutton(t_frame,text='减法',variable=vara,value='2',command=rdoprt)
+ra2.pack(anchor=tk.W,side=tk.LEFT)
+ra3=tk.Radiobutton(t_frame,text='乘法',variable=vara,value='3',command=rdoprt)
+ra3.pack(anchor=tk.W,side=tk.LEFT)
+ra4=tk.Radiobutton(t_frame,text='除法',variable=vara,value='4',command=rdoprt)
+ra4.pack(anchor=tk.W,side=tk.LEFT)
+ra1.select()
+
+
+
+###########口算题类型选择############
+varb = tk.StringVar()
+varb.set("L")  # initialize
+rb1=tk.Radiobutton(l_frame,text='随机',variable=varb,value='1',command=rdoprt)
+rb1.pack(anchor=tk.W,side=tk.LEFT)
+rb2=tk.Radiobutton(l_frame,text='加法进位',variable=varb,value='2',command=rdoprt)
+rb2.pack(anchor=tk.W,side=tk.LEFT)
+rb3=tk.Radiobutton(l_frame,text='加法无进位',variable=varb,value='3',command=rdoprt)
+rb3.pack(anchor=tk.W,side=tk.LEFT)
+rb4=tk.Radiobutton(l_frame,text='减法退位',variable=varb,value='4',command=rdoprt)
+rb4.pack(anchor=tk.W,side=tk.LEFT)
+rb5=tk.Radiobutton(l_frame,text='减法无退位',variable=varb,value='5',command=rdoprt)
+rb5.pack(anchor=tk.W,side=tk.LEFT)
+rb1.select()
+
+
+
+
+
+
+
+
+
+
 def main():
     print('程序开始运行')
+    root.mainloop()
     sets = 3  # 三套题
     psm_list =[]
 
@@ -44,10 +129,10 @@ def main():
 
     # 配置每套题包括的内容，至少有一种题型，每套题的总数量为几种题型的数量总合。
     psm_type = [
-        [1, (0, 20), 1, 1, (0, 1), True, False, 25],
-        [2, (0, 20), 1, 1, (0, 1), True, False, 25],
+        [1, (0, 20), 2, 1, (0, 1), True, False, 25],
+        [2, (0, 20), 3, 1, (0, 1), True, False, 25],
         [3, (0, 9), 1, 1, (0,), True, False, 25],
-        [4, (0, 81), 1, 2, (0, 1), True, False, 25]
+        [4, (0, 81), 1, 1, (0, 1), True, False, 25]
     ]
 
     #循环生成每套题
--- Psmrcddup.py
+++ Psmrcddup.py
@@ -31,6 +31,7 @@
 
 import random
 
+
 class Generator(object):
     '''
     - @sigunm   int
@@ -38,7 +39,7 @@ class Generator(object):
     - @range    tuple
         随机范围, 默认: (0, 10)
     - @need_carry    int
-        进位, 退位运算(1: 随机, 2: 进位, 3: 退位), 默认: 1
+        进位, 退位运算(1: 随机, 2: 进位, 3: 退位, 4:不进位也不退位), 默认: 1
         进位: 加法和乘法运算时, 才会产生进位
         退位: 减法和出发运算时, 才会产生退位
     - @step int
@@ -62,28 +63,31 @@ class Generator(object):
     step = None
     filter = None
     same = None
-    intFlag = None
-    number =None
+    # intFlag = None
+    number = None
 
-    def __init__(self, signum=None, range=(0, 10), need_carry=1, step=1, filter=(0, 10), same=True, intFlag=False,num=80):
+    def __init__(self, signum=None, range=(0, 10), need_carry=1, step=1, filter=(0, 10), same=True,
+                 num=80):
         if signum is None:
             raise Exception("required param signum is missing or signum is None")
-        if signum not in (1,2,3,4):
+        if signum not in (1, 2, 3, 4):
             raise Exception("param signum must be 1 or 2 or 3 or 4")
         if range is None:
             raise Exception("required param range is missing or range is None")
-        if need_carry not in (1, 2, 3):
+        if need_carry not in (1, 2, 3, 4):
             need_carry = 1
         # step 参数暂时默认为 1
         step = 1
-        if (signum == 1 or signum == 3) and need_carry == 3:
-            raise Exception("非法配置参数, 加法和乘法运算不会产生退位")
+        if (signum == 1 or signum == 3 or signum == 4) and need_carry == 3:
+            raise Exception("非法配置参数, 加法和乘法运算不会产生退位，除法暂时不考虑退位")
         if (signum == 2 or signum == 4) and need_carry == 2:
             raise Exception("非法配置参数, 减法和除法运算不会产生进位")
+        if (signum == 3 or signum == 4) and need_carry == 4:
+            raise Exception("非法配置参数, 乘法和除法运算不暂时不锁定不进位不退位")
 
-        self.__init(signum, range, need_carry, step, filter, same, intFlag, num)
+        self.__init(signum, range, need_carry, step, filter, same, num)
 
-    def __init(self, signum=None, range=(0, 10), need_carry=1, step=1, filter=(0, 10), same=True, intFlag=2, num=80):
+    def __init(self, signum=None, range=(0, 10), need_carry=1, step=1, filter=(0, 10), same=True,num=80):
         '''初始化参数配置'''
         if signum == 1:
             self.signum = "+"
@@ -98,14 +102,15 @@ class Generator(object):
         self.need_carry = need_carry
         self.step = step
         self.filter = filter
-        self.intFlag = intFlag
 
+        self.same =same
         self.min = min(range)
         self.max = max(range)
         self.number = num
-        self.__data_list = []#生成的口算题
-    
+        self.__data_list = []  # 生成的口算题
+
     def __is_int(self, num):
+        '''判断一个数是否为整数'''
         num_str = str(num)
         num_strs = num_str.split(".")
         if len(num_strs) == 1:
@@ -116,40 +121,39 @@ class Generator(object):
 
     def __is_valid(self, a, b):
         '''校验生成数值是否符合配置要求'''
-        if self.need_carry == 1:
-            if self.signum == "/":
+        if self.need_carry == 1:  # 随机无论进退位时的判断
+            # 加法与乘法无所谓
+            if self.signum == "+" or self.signum == "*":
+                return True
+
+            if self.signum == "-" and a > b:  # 避免减法出现结果为0或负数的题型
+                return True
+
+            if self.signum == "/" and a > b:  # 修改判断除法结果大于1 避免出现10/10 20/20 这种题型。
                 r = eval("{}{}{}".format(a, self.signum, b))
-                if self.intFlag == False:
-                    is_int = self.__is_int(r)
-                    if self.signum == "-" and r > 1:#修改成大于1 避免出现10/10 20/20 这种题型。
-                        return True
-                    elif self.signum == "/" and is_int:
-                        return True
-                    else:
-                        return False
-                else:
-                    return True
-            return True
-        if self.need_carry == 2 and (self.signum == "+" or self.signum == "*") and ( len(str(eval("{}{}{}".format(a, self.signum, b)))) > max(len(str(a)), len(str(b))) ):
-            return True
-        elif self.need_carry == 3 and (self.signum == "-" or self.signum == "/"):
-            r = eval("{}{}{}".format(a, self.signum, b))
-            if self.intFlag == False:
-                is_int = isinstance(r, int)
-                if self.signum == "-" and r > 0 and len(str(r)) < min(len(str(a)), len(str(b))):
-                    return True
-                elif self.signum == "/" and is_int and len(str(r)) < min(len(str(a)), len(str(b))) :
+                is_int = self.__is_int(r)
+                if self.signum == "/" and is_int:  # 必须是整数
                     return True
                 else:
                     return False
             else:
-                # 此处还需判断是否退位
-                is_int = isinstance(r, int)
-                if is_int and len(str(r)) < min(len(str(a)), len(str(b))):
-                    return True
-                elif is_int == False and len(str(int(r))) < min(len(str(a)), len(str(b))):
-                    return True
                 return False
+
+        # 选定加法不进位或减法不退位的判断
+        if self.need_carry == 4:
+            if self.signum == "+":
+                return self.__is_nocarry(a, b)
+
+            if a>b and self.signum == "-":
+                return self.__is_nocarry(a, b)
+
+        # 加法和乘法必须为进位时的判断
+        if self.need_carry == 2 and (self.signum == "+" or self.signum == "*") and self.__is_carry(a, b):
+            return True
+        # 减法必须为退位的判断
+        if self.need_carry == 3 and self.signum == "-" and a > b and self.__is_s_nocarry(a, b):
+            return True
+
         else:
             return False
 
@@ -160,16 +164,43 @@ class Generator(object):
         return number - value0 * 10
 
     def __is_carry(self, a, b):
-        '''判断加法是否存在进位'''
-        if (self.__get_num(a) + self.__get_num(b) < 10):
-            return False
-        else:
-            return True
+        '''判断加法和乘法存在进位'''
+        if self.signum == '+':
+            if (self.__get_num(a) + self.__get_num(b) < 10):
+                return False
+            else:
+                return True
+        if self.signum == '*':
+            if (self.__get_num(a) * self.__get_num(b) < 10):
+                return False
+            else:
+                return True
+
+    def __is_nocarry(self, a, b):
+        '''判断加法无进位，减法无退位'''
+        if self.signum == '+':
+            if (self.__get_num(a) + self.__get_num(b) < 10):
+                return True
+            else:
+                return False
+        if self.signum == '-':
+            if (self.__get_num(a) >= self.__get_num(b)):
+                return True
+            else:
+                return False
+
+    def __is_s_nocarry(self, a, b):
+        '''判断减法有退位'''
+        if self.signum == '-':
+            if (self.__get_num(a) < self.__get_num(b)):
+                return True
+            else:
+                return False
 
     def __get_topic(self, a, b):
         '''根据两个数字返回一道单步口算题'''
         if self.__is_valid(a, b):
-            if(self.signum == '*'):
+            if (self.signum == '*'):
                 return "{}{}{}=".format(a, '×', b)
             if (self.signum == '/'):
                 return "{}{}{}=".format(a, '÷', b)
@@ -178,6 +209,9 @@ class Generator(object):
 
     def generate_data(self):
         '''根据条件生成所需数据列表'''
+
+        slist = []
+
         # 循环生成所有加法口算题
         for i in range(self.min, self.max):
             for j in range(self.min, self.max):
@@ -185,47 +219,59 @@ class Generator(object):
                     continue
                 addt = self.__get_topic(i, j)
                 if addt:
-                    self.__data_list.append(addt)
-        if (len(self.__data_list) >= self.number):
-            random.shuffle(self.__data_list)  # 洗牌，先打乱list中的排序
-            self.__data_list = random.sample(self.__data_list, self.number)  # 随机取需要的口算题量。
-        elif(len(self.__data_list) == 0):
+                    slist.append(addt)
+
+        if (len(slist) >= self.number):
+            random.shuffle(slist)  # 洗牌，先打乱list中的排序
+            self.__data_list = random.sample(slist, self.number)  # 随机取需要的口算题量。
+
+            return self.__data_list
+        if (len(slist) == 0):
             raise Exception('此数字范围内生成的加法口算题未能达到您要求的数目，请检查配置以适合程序的生成，请修改数值符合加法进位')
         else:
             if self.same:
-                for i in range(self.number - len(self.__data_list)):
-                    k = random.randint(0, len(self.__data_list) - 1)
-                    self.__data_list.append(self.__data_list[k])
+                for i in range(self.number - len(slist)):
+                    k = random.randint(0, len(slist)-1)
+                    slist.append(slist[k])
+                return slist
             else:
+
                 raise Exception('此数字范围内生成的加法口算题未能达到您要求的数目，请检查配置以适合程序的生成，比如设置可以生成相同的题')
-        return self.__data_list
+
+
 
     def produce(self):
         '''打印预览预留接口'''
         pass
 
-
+    def test(self):
+        print(self.__is_nocarry(9,9))
 def main():
     data_list = []
     # 生成加法进位口算题
-    g_add = Generator(signum=1, range=(0, 20), need_carry=2, step=1, filter=(0, 1), same=True, num=20)
+    g_add = Generator(signum=1, range=(0, 20), need_carry=1, step=1, filter=(0, 1), same=True, num=20)
     g_add_data = g_add.generate_data()
-    data_list.extend(g_add_data)
+    print(g_add_data)
+    # data_list.extend(g_add_data)
     # 生成减法退位口算题
-    g_sub = Generator(signum=2, range=(0, 20), need_carry=3, step=1, filter=(0, 1), same=True, num=20)
+    g_sub = Generator(signum=2, range=(0, 20), need_carry=1, step=1, filter=(0, 1), same=True, num=20)
     g_sub_data = g_sub.generate_data()
-    data_list.extend(g_sub_data)
+    print(g_sub_data)
+    # data_list.extend(g_sub_data)
     # # 生成乘法口算题
-    g_multi = Generator(signum=3, range=(0, 8), need_carry=1, step=1, filter=(0,), same=True, num=20)
+    g_multi = Generator(signum=3, range=(0, 8), need_carry=2, step=1, filter=(0,1), same=True, num=20)
     g_multi_data = g_multi.generate_data()
-    data_list.extend(g_multi_data)
-    # # 生成除法口算题
+    print(g_multi_data)
+    # data_list.extend(g_multi_data)
+    # 生成除法口算题
     g_div = Generator(signum=4, range=(0, 81), need_carry=1, step=1, filter=(0, 1), same=True, num=20)
     g_div_data = g_div.generate_data()
-    data_list.extend(g_div_data)
+    print(g_div_data)
+    # data_list.extend(g_div_data)
     # 打印数据
-    print(data_list)
-    
+    # print(data_list)
+    g_add.test()
+
 
 if __name__ == '__main__':
     main()  # 程序入口
