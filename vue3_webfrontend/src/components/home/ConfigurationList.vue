623bb5e50a97e8b53546537cf6e9ca7ed757f3ba
kk <kkplusplus@163.com>
2023-04-13 20:34:34 +0800

    feat: 配置列表样式美化

--- vue3_webfrontend/src/components/home/ConfigurationList.vue
+++ vue3_webfrontend/src/components/home/ConfigurationList.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <p>已保存配置列表</p>
-    <ElCard v-for="c in configurations" style="margin-bottom: 20px;" :shadow="'hover'" @click="handle(c.id)">
-      <!-- 是否选中 -->
-      {{ c.name }}
-      <el-icon v-if="configurations && configurations.length > 1" @click="remove(c.id)">
-        <CircleCloseFilled />
-      </el-icon>
+    <ElCard v-for="c in configurations" style="margin-bottom: 20px;" class="card" :shadow="'hover'" @click="handle(c.id)">
+      <div class="card-item">
+        <!-- 是否选中 -->
+        {{ c.name }}
+        <el-icon v-if="configurations && configurations.length > 1" @click.stop="remove(c.id)">
+          <CircleCloseFilled />
+        </el-icon>
+      </div>
     </ElCard>
   </div>
 </template>
 
 <script setup>
-import { getCurrentInstance } from 'vue';
+import { getCurrentInstance, ref } from 'vue';
 import configStorage from '@/utils/configStorage';
 import { cloneDeep } from 'lodash';
 
@@ -24,6 +26,8 @@ const props = defineProps({
 
 const emits = defineEmits(['removed', 'selected'])
 
+const activeConfigurationId = ref('')
+
 const remove = (id) => {
   new configStorage().remove(id)
   proxy.$message.success('删除成功!')
@@ -34,6 +38,7 @@ const handle = async (id) => {
   try {
     await proxy.$messageBox.confirm('确定加载吗? 注意未保存的参数将会丢失!', '提示', { type: 'warning' })
     const c = props.configurations.find(p => p.id == id)
+    activeConfigurationId.value = id
     emits('selected', cloneDeep(c))
   } catch (error) {
 
@@ -42,4 +47,23 @@ const handle = async (id) => {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+
+  &.active {
+    background-color: aqua;
+  }
+
+  .card-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+
+    i {
+      font-size: 20px;
+      color: blue;
+    }
+  }
+}
+</style>
