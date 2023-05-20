997ac067f878849aa77327f178a6ae6eaf2de3c0
J.sky <bosichong@qq.com>
2023-05-20 18:13:13 +0800

    在底部添加打赏。

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,69 @@
 <template>
-    <footer class="py-6 px-7 flex justify-center flex-wrap items-baseline">
-        <template v-for="bind in bands">
-            <p class="h-7">
-                <a v-if="bind.link" :href="bind.link" target="_blank">
-                    <img :alt="bind.imageAlt" :src="bind.imageSrc" class="mr-2" />
-                </a>
-                <img v-else :alt="bind.imageAlt" :src="bind.imageSrc" class="mr-2" />
-            </p>
-        </template>
-    </footer>
+  <footer class="py-6 px-7 flex justify-center flex-wrap items-baseline">
+    <el-header>
+      <el-tooltip
+          class="box-item"
+          content="赞赏作者一杯咖啡！"
+          placement="top"
+      >
+        <el-button type="success" :icon="Coffee" circle @click="centerDialogVisible = true" />
+      </el-tooltip>
+
+      <el-dialog v-model="centerDialogVisible" title="请作者们喝咖啡：）" width="30%" center>
+        <el-row :gutter="20">
+          <el-col :span="12"><div class="grid-content ep-bg-purple" /><img src="@/assets/wx.png" alt="请作者喝咖啡：）"></el-col>
+          <el-col :span="12"><div class="grid-content ep-bg-purple" /><img src="@/assets/zfb.png" alt="请作者喝咖啡：）"></el-col>
+        </el-row>
+        <div>
+
+
+        </div>
+      </el-dialog>
+
+    </el-header>
+    <el-footer>
+      <template v-for="bind in bands">
+        <p class="h-7">
+          <a v-if="bind.link" :href="bind.link" target="_blank">
+            <img :alt="bind.imageAlt" :src="bind.imageSrc" class="mr-2"/>
+          </a>
+          <img v-else :alt="bind.imageAlt" :src="bind.imageSrc" class="mr-2"/>
+        </p>
+      </template>
+    </el-footer>
+
+  </footer>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
+import {
+  Coffee,
+} from '@element-plus/icons-vue'
+
+const centerDialogVisible = ref(false)
+
+
+
 
 const bands = ref([
-    { imageAlt: "python-3.8.8", imageSrc: "https://img.shields.io/badge/Python-3.8.8-green?logo=python" },
-    { imageAlt: "fastAPI-0.85.1", imageSrc: "https://img.shields.io/badge/fastAPI-0.85.1-green" },
-    { imageAlt: "Vue-3.2.41", imageSrc: "https://img.shields.io/badge/Vue-3.2.41-blue" },
-    { imageAlt: "license-Apache--2.0", imageSrc: "https://img.shields.io/badge/license-Apache--2.0-green" },
-    { imageAlt: "", imageSrc: "https://img.shields.io/badge/Gitee--PrimarySchoolMathematics-red?logo=gitee", link: 'https://gitee.com/J_Sky/PrimarySchoolMathematics' },
-    { imageAlt: "", imageSrc: "https://img.shields.io/badge/Github--PrimarySchoolMathematics-green?logo=github", link: 'https://github.com/bosichong/PrimarySchoolMathematics' }
+  {imageAlt: "Vue-3.2.41", imageSrc: "https://img.shields.io/badge/Vue-3.2.41-blue"},
+  {imageAlt: "license-Apache--2.0", imageSrc: "https://img.shields.io/badge/license-Apache--2.0-green"},
+  {
+    imageAlt: "",
+    imageSrc: "https://img.shields.io/badge/Gitee--PrimarySchoolMathematics-red?logo=gitee",
+    link: 'https://gitee.com/J_Sky/PrimarySchoolMathematics'
+  },
+  {
+    imageAlt: "",
+    imageSrc: "https://img.shields.io/badge/Github--PrimarySchoolMathematics-green?logo=github",
+    link: 'https://github.com/bosichong/PrimarySchoolMathematics'
+  }
 ])
 </script>
 
 <style lang="scss" scoped>
 footer {
-    background: #f0f2f5;
+  background: #f0f2f5;
 }
 </style>
