91262ede462a4b0d62137e5b29a05614ee8bb18e
kk <kkplusplus@163.com>
2023-05-22 21:31:55 +0800

    build: 为github pages构建

---
+++ docs/assets/app.dcc67871.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index.820e40f0.js";const s=a("app",{state:()=>({printPreviewPapers:[]}),actions:{navigateToPrint(t,e,p){this.$patch(r=>{r.printPreviewPapers=p,t.push({path:"/print",query:{fileName:e}})})}}});export{s as u};
