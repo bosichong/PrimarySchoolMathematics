bec71e57675492af4c281e5ce25294386672b8d2
J.sky <bosichong@qq.com>
2023-05-22 18:45:20 +0800

    解决了打包element打包button样式失败的问题。

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from "pinia"
-import App from './App.vue'
-import { router } from './router';
-import ElementPlus from 'element-plus'
-import "./styles/tailwind.css";
-
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import "./styles/tailwind.css";
 import "./styles/shared.scss";
 import 'element-plus/dist/index.css'
-
+import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import App from './App.vue'
+import { router } from './router';
 
 const pinia = createPinia()
 const app = createApp(App);
