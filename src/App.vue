9e870706337ff98b496214b771b5156d01f43059
zhoukan <kkplusplus@163.com>
2023-05-24 16:55:24 +0800

    fix: [I77PH2]生成口算题后点击打印，在打印页面点击返回之后，之前的口算题设置的配置参数会全部丢失，需要重新设置口算题配置项。

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition>
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
+  </router-view>
 </template>
 
 <script setup>
